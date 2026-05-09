d0f92ac7723192020b7ff218f49d044193affaf4
gaurangpatil97 <gaurangpatil9@gmail.com>
2026-05-09 18:08:05 +0530

    chore: secure API keys with dotenv and add .env.example

--- models/HelmetDetection/downloadDataset.py
+++ models/HelmetDetection/downloadDataset.py
@@ -6,10 +6,14 @@
                 
 
 
+import os
+from dotenv import load_dotenv
 from roboflow import Roboflow
 
+load_dotenv(r"C:\Users\gaura\Desktop\Sentinel\.env")
+
 try:
-    rf = Roboflow(api_key="gj1dDXzQnsRNBuYJCmoJ")
+    rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
     workspace = rf.workspace("gaurang-patil-gr0h9")
     project = workspace.project("helmet-detection-ntbfz-yozkl")
     version = project.version(1)
